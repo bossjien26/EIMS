0229971d93d686ebd372e789f6603e95c15ad162
jan <bossjien26@gmail.com>
2022-03-01 23:59:07 +0800

    feat:add OrderFlagEnum

---
+++ src/Enum/OrderFlagEnum.cs
@@ -0,0 +1,25 @@
+namespace Enum
+{
+    /// <summary>
+    /// order flag
+    /// </summary>
+    public enum OrderFlagEnum
+    {
+        /// <summary>
+        /// 銷售單
+        /// </summary>
+        SaleOrder = 1,
+
+        /// <summary>
+        /// 採購單
+        /// </summary>
+        PurchaseOrder = 2,
+
+        /// <summary>
+        /// 訂購單
+        /// </summary>
+        DeliveryOrder = 3,
+
+        Return = 4
+    }
+}
