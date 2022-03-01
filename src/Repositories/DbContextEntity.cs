eddb80654c76ca865fe1e12123a87ccd9acfb343
jan <bossjien26@gmail.com>
2022-03-01 23:59:32 +0800

    feat:add entity

--- src/Repositories/DbContextEntity.cs
+++ src/Repositories/DbContextEntity.cs
@@ -1,21 +1,54 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using Entities;
+using System;
 
 namespace DbEntity
 {
     public class DbContextEntity : DbContext
     {
         public IDbContextTransaction _transaction;
-        public DbContextEntity(DbContextOptions<DbContextEntity> options)
-        : base(options) { }
+        public DbContextEntity(DbContextOptions<DbContextEntity> options) : base(options) { }
 
-        
         #region user
 
         public DbSet<User> Users { get; set; }
 
         public DbSet<UserDetail> UserDetails { get; set; }
+
+        #endregion
+
+        #region Order
+
+        public DbSet<OrderInfo> OrderInfos { get; set; }
+
+        public DbSet<OrderItemDetail> OrderItemDetails { get; set; }
+
+        #endregion
+
+        #region Item  
+
+        public DbSet<Warehouse> Warehouses { get; set; }
+
+        public DbSet<Item> Items { get; set; }
+
+        #endregion
+
+        #region Company
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<ClientContact> ClientContacts { get; set; }
+
+        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<SupplierContact> SupplierContacts { get; set; }
+
+        #endregion
+
+        #region  Other
+
+        public DbSet<Currency> Currencies { get; set; }
 
         #endregion
 
@@ -46,9 +79,46 @@ namespace DbEntity
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-        
-            modelBuilder.Entity<User>().Property(d => d.CreateAt).ValueGeneratedOnAdd();
-            modelBuilder.Entity<User>().Property(d => d.UpdateAt).ValueGeneratedOnAddOrUpdate();
+
+            modelBuilder.Entity<User>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<User>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<OrderInfo>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<OrderInfo>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<OrderItemDetail>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<OrderItemDetail>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<Item>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Item>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<Supplier>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Supplier>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<SupplierContact>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<SupplierContact>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<Client>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<Client>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
+
+            modelBuilder.Entity<ClientContact>().Property(d => d.CreateAt).HasDefaultValueSql("GETDATE()")
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<ClientContact>().Property(d => d.UpdateAt).HasDefaultValueSql("GETUTCDATE()")
+                .ValueGeneratedOnUpdate();
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
