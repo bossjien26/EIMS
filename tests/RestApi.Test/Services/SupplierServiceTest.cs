5bff5ebdb9005cddc076cf3494f89e20048b5d60
jan <bossjien26@gmail.com>
2022-03-04 23:32:05 +0800

    test:SupplierServiceTest

---
+++ tests/RestApi.Test/Services/SupplierServiceTest.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+using Moq;
+using NUnit.Framework;
+using Repositories.Interface;
+using RestApi.Test.DatabaseSeeders;
+using Services;
+
+namespace RestApi.Test.Services
+{
+    [TestFixture]
+    public class SupplierServiceTest
+    {
+        private readonly Mock<Supplier> _entityMock;
+
+        private readonly Mock<ISupplierRepository> _repoMock;
+
+        public SupplierServiceTest()
+        {
+            _entityMock = new Mock<Supplier>();
+
+            _repoMock = new Mock<ISupplierRepository>();
+        }
+
+        [Test]
+        async public Task ShouldGetById()
+        {
+            // Arrange
+            _repoMock.Setup(r => r.Get(x => x.Id == 1))
+                .Returns(Task.FromResult(_entityMock.Object));
+
+            // Act
+            var result = await new SupplierService(_repoMock.Object).GetById(1);
+
+            // Assert
+            Assert.IsInstanceOf<Supplier>(result);
+        }
+
+        [Test]
+        public void ShouldGetMany()
+        {
+            //Arrange
+            var suppliers = SupplierSeeder.SeedMany(10, 15).AsQueryable();
+
+            _repoMock.Setup(u => u.GetAll()).Returns(suppliers);
+
+            //Atc
+            var result = new SupplierService(_repoMock.Object).GetMany(1, 5).ToList();
+
+            //Assert
+            Assert.IsInstanceOf<List<Supplier>>(result);
+            Assert.AreEqual(5, result.Count);
+        }
+
+        [Test]
+        public void ShouldInsertOne()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.DoesNotThrowAsync(() =>
+                new SupplierService(_repoMock.Object).Insert(_entityMock.Object)
+            );
+        }
+
+        [Test]
+        public void ShouldUpdate()
+        {
+            //Arrange
+            //Act
+            //Asset
+            _repoMock.Setup(c => c.Get(x => x.Name == "aaƒ"))
+                .Returns(Task.FromResult(_entityMock.Object));
+
+            _entityMock.Object.Name = "test";
+
+            Assert.DoesNotThrowAsync(() =>
+                new SupplierService(_repoMock.Object).Update(_entityMock.Object)
+            );
+        }
+    }
+}
