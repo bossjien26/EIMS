3c4b7c27f25644d7105a7f67040e2ba8db4f458b
jan <bossjien26@gmail.com>
2022-03-05 21:10:38 +0800

    feat:change sort

--- src/RestApi/Startup.cs
+++ src/RestApi/Startup.cs
@@ -23,12 +23,12 @@ namespace RestApi
 {
     public class Startup
     {
+        public IConfiguration _configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
         }
-
-        public IConfiguration _configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
