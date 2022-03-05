28115e4d5f7cfecef2807f9d3c1d4c4762b0e40b
jan <bossjien26@gmail.com>
2022-03-05 21:52:47 +0800

    feat:save exchange rate

--- src/ScheduleJob/Job/ExchangeRateJob.cs
+++ src/ScheduleJob/Job/ExchangeRateJob.cs
@@ -1,6 +1,10 @@
+using DbEntity;
+using Entities;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using Quartz;
+using Services;
+using Services.Interface;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,17 +13,21 @@ using System.Threading.Tasks;
 public class ExchangeRateJob : IJob
 {
     private readonly ILogger<ExchangeRateJob> _logger;
-    public ExchangeRateJob(ILogger<ExchangeRateJob> logger)
+
+    private ICurrencyService _currencyService;
+
+    public ExchangeRateJob(ILogger<ExchangeRateJob> logger,
+     DbContextEntity dbContextEntity)
     {
         _logger = logger;
+        _currencyService = new CurrencyService(dbContextEntity);
     }
 
     public Task Execute(IJobExecutionContext context)
     {
         _logger.LogInformation("get exchange rate");
-
         var currencies = GetCurrency();
-
+        TransferRate(currencies);
         _logger.LogInformation("success");
 
         return Task.CompletedTask;
@@ -30,19 +38,56 @@ public class ExchangeRateJob : IJob
         var requestURL = "https://api.exchangerate.host/latest";
         var httpRequest = (HttpWebRequest)WebRequest.Create(requestURL);
 
-        // httpRequest.Accept = "application/xml";
-
-
         var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
         var streamReader = new StreamReader(httpResponse.GetResponseStream());
         var result = streamReader.ReadToEnd();
 
         var jObject = JObject.Parse(result);
         return JObject.Parse(jObject["rates"].ToString());
+    }
 
-        // foreach (var item in exchangeRate)
-        // {
-        //     Console.WriteLine("currency : {0} , rate : {1}", item.Key, item.Value);
-        // }
+    private void TransferRate(JObject exchangeRates)
+    {
+        foreach (var exchangeRate in exchangeRates)
+        {
+            if (exchangeRate.Key != "TWD")
+            {
+                TransferTWDToRate(exchangeRate.Key);
+            }
+        }
+    }
+
+    private void TransferTWDToRate(string exchangeRate)
+    {
+        var requestURL = "https://api.exchangerate.host/convert?from=TWD&to=" + exchangeRate;
+        var httpRequest = (HttpWebRequest)WebRequest.Create(requestURL);
+
+        var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+        var streamReader = new StreamReader(httpResponse.GetResponseStream());
+        var result = streamReader.ReadToEnd();
+
+        var jObject = JObject.Parse(result);
+        SaveRate(exchangeRate, float.Parse(jObject["result"].ToString()));
+    }
+
+    private void SaveRate(string exchangeRate, float rate)
+    {
+        var currency = _currencyService.GetByCurrencyName(exchangeRate);
+        _logger.LogInformation("save {0}", exchangeRate);
+
+        if (currency == null)
+        {
+            _currencyService.Insert(
+            new Currency()
+            {
+                Name = exchangeRate,
+                ExchangeRate = rate
+            });
+        }
+        else
+        {
+            currency.Result.ExchangeRate = rate;
+            _currencyService.Update(currency.Result);
+        }
     }
 }
