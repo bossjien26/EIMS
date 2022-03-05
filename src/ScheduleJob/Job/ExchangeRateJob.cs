8aa067b1117d236b6207a110cbb45745d0dcc103
jan <bossjien26@gmail.com>
2022-03-05 16:53:15 +0800

    feat:add get currencies

--- src/ScheduleJob/Job/ExchangeRateJob.cs
+++ src/ScheduleJob/Job/ExchangeRateJob.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using Quartz;
 using System.IO;
 using System.Net;
@@ -16,28 +17,32 @@ public class ExchangeRateJob : IJob
     public Task Execute(IJobExecutionContext context)
     {
         _logger.LogInformation("get exchange rate");
+
+        var currencies = GetCurrency();
+
+        _logger.LogInformation("success");
+
+        return Task.CompletedTask;
+    }
+
+    private JObject GetCurrency()
+    {
         var requestURL = "https://api.exchangerate.host/latest";
         var httpRequest = (HttpWebRequest)WebRequest.Create(requestURL);
 
-        httpRequest.Accept = "application/xml";
+        // httpRequest.Accept = "application/xml";
 
 
         var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
         var streamReader = new StreamReader(httpResponse.GetResponseStream());
         var result = streamReader.ReadToEnd();
 
-        // Console.WriteLine(httpResponse.StatusCode);
-        _logger.LogInformation(result);
-        // var request = new XMLHttpRequest();
-        // request.open('GET', requestURL);
-        // request.responseType = 'json';
-        // request.send();
+        var jObject = JObject.Parse(result);
+        return JObject.Parse(jObject["rates"].ToString());
 
-        // request.onload = function() {
-        //     var response = request.response;
-        //     console.log(response);
+        // foreach (var item in exchangeRate)
+        // {
+        //     Console.WriteLine("currency : {0} , rate : {1}", item.Key, item.Value);
         // }
-
-        return Task.CompletedTask;
     }
 }
